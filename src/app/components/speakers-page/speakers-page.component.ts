b55cdec9baa1ac1f080739765298a863da53496a
Henry Taylor <henrytaylor@live.co.uk>
2022-01-23 15:53:09 +0000

    feat(speakers-page): component added for speakers list

---
+++ src/app/components/speakers-page/speakers-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speakers-page',
+  templateUrl: './speakers-page.component.html',
+  styleUrls: ['./speakers-page.component.scss']
+})
+export class SpeakersPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
