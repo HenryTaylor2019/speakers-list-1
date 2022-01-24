3366149367fdb428783d4bccfe5a1bc4cc029813
Henry Taylor <henrytaylor@live.co.uk>
2022-01-24 17:14:02 +0000

    fix(select-bar): countries in alphabetical order - no longer showing duplicates

--- src/app/speakers-app/home/home.component.ts
+++ src/app/speakers-app/home/home.component.ts
@@ -1,19 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-    constructor(
-        private route: ActivatedRoute,
-    ) {
-    }
-
-    ngOnInit(): void {
-    }
+export class HomeComponent  {
 
 }
